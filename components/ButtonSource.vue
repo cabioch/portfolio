3bf6381d2d335e9adf3ddf138e8799c758312d92
Enzo Cabioch <enzocabioch1@gmail.com>
2022-01-21 21:11:41 +0100

    Add transition and condition on button

--- components/ButtonSource.vue
+++ components/ButtonSource.vue
@@ -1,9 +1,20 @@
 <template >
-  <v-tooltip bottom v-if="isVisible">
+  <v-tooltip bottom>
     <template v-slot:activator="{ on }">
-      <v-btn fab fixed :href="url" target="_blank" class="btn-github" v-on="on">
-        <v-icon>mdi-github</v-icon>
-      </v-btn>
+      <v-fade-transition hide-on-leave>
+        <v-btn
+          fab
+          fixed
+          :href="url"
+          target="_blank"
+          class="btn-github"
+          v-on="on"
+          v-show="isVisible"
+          transition="scale-transition"
+        >
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+      </v-fade-transition>
     </template>
     <span>Voir mon code source sur GitHub</span>
   </v-tooltip>
@@ -19,7 +30,6 @@ export default {
 </script>
 
 <style scoped>
-/* TODO Afficher progressivement le bouton */
 .btn-github {
   top: 90vh;
   left: 95vw;
